f835e6e97dae919a6d9350104a4bcedd26ebcbaf
Binoy Das <binoyd@amazon.com>
2020-07-21 00:07:09 -0700

    Minimum height triangle

---
+++ Mathematics/MinimumHeightTriangle.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+import os
+import sys
+import math
+os.environ['OUTPUT_PATH'] = './MinimumHeightTriangle.txt'
+
+def lowestTriangle(base, area):
+    return math.ceil(float(2*area)/base)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        base, area = input().split()
+
+        base, area = [int(base), int(area)]
+
+        height = lowestTriangle(base, area)
+
+        fptr.write(str(height) + '\n')
+
+    fptr.close()
